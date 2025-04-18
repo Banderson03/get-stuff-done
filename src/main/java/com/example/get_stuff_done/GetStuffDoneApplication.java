dc2796e0f3b3867881de1410192deaf656824f8e
10625639 <cmoloney2@huskers.unl.edu>
2025-04-18 15:53:27 -0500

    Initial HelloWorld Changes

---
+++ src/main/java/com/example/get_stuff_done/GetStuffDoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.get_stuff_done;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetStuffDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetStuffDoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/get_stuff_done/GetStuffDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.get_stuff_done;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetStuffDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
