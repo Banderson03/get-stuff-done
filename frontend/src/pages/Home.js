02be755f87be9cb377d5bdd14ea7f4e0e890cce8
Banderson03 <bennett.scott.anderson@gmail.com>
2025-04-23 14:10:34 -0500

    Added hello fetch

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,10 +4,36 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
     const navigate = useNavigate();
+    const [message, setMessage] = useState(""); // State to hold the message fetched from the backend
+
+    useEffect(() => {
+
+        const fetchMessage = () => {
+            fetch("/api/hello")
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Failed to fetch data");
+                    }
+                    return response.text();
+                })
+                .then((data) => {
+                    setMessage(data); // Update the state with the fetched message
+                })
+                .catch((error) => {
+                    console.error("Error fetching data:", error);
+                });
+        };
+
+        const intervalId = setInterval(fetchMessage, 250);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
 
     return (
         <div className="home">
-            <h1>Get Started With Get Stuff Done</h1>
+            <h1>Git Started With Git Stuff Done</h1>
+            <p>{message}</p>
         </div>
     )
 }
